95dd9a64a4c1979b5ace5ae09842038de79be184
Fernand Pajot <fernand@epigram.me>
2025-01-14 13:04:56 -0700

    Removing newlines to see if that helps

--- clean_opinions_csv.py
+++ clean_opinions_csv.py
@@ -7,7 +7,12 @@ df = pl.scan_csv(
     input_file,
     separator=",",
     quote_char="`",
-    ignore_errors=True  # Skip malformed rows
-)
-
-df.sink_csv(output_file)
+    ignore_errors=False,
+).with_columns([
+    pl.col("plain_text").str.replace_all("\n", "\/n"),
+    pl.col("html").str.replace_all("\n", "\/n"),
+    pl.col("html_lawbox").str.replace_all("\n", "\/n"),
+    pl.col("html_columbia").str.replace_all("\n", "\/n"),
+    pl.col("html_with_citations").str.replace_all("\n", "\/n"),
+    pl.col("extracted_by_ocr").str.replace_all("\n", "\/n"),
+]).sink_csv(output_file, quote_char="`")
