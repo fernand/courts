af359673f6665f10526fea1833992dc674ac2807
Fernand Pajot <fernand@epigram.me>
2025-01-15 11:05:26 -0700

    .

--- clean_schema.sql
+++ clean_schema.sql
@@ -248,10 +248,7 @@ CREATE UNLOGGED TABLE public.search_parenthetical (
 CREATE UNIQUE INDEX idx_search_opinioncluster_id on public.search_opinioncluster (id);
 CREATE INDEX idx_search_opinioncluster_docket_id on public.search_opinioncluster (docket_id);
 CREATE UNIQUE INDEX idx_search_docket_id ON public.search_docket (id);
-CREATE INDEX idx_search_docket_date_modified ON public.search_docket (date_modified);
-CREATE INDEX idx_search_docket_date_created ON public.search_docket (date_created);
 CREATE INDEX idx_search_docket_date_filed ON public.search_docket (date_filed);
 CREATE INDEX idx_hash_search_docket_court_id ON public.search_docket USING hash (court_id);
 CREATE UNIQUE INDEX idx_search_opinion_id on public.search_opinion (id);
 CREATE INDEX idx_search_opinion_cluster_id on public.search_opinion (cluster_id);
-CREATE INDEX idx_search_opinion_date_created on public.search_opinion (date_created);
